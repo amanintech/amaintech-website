7dd91648989ef16ad94a341cae4d016708c84120
Aman Sharma <amanrazdan@amp.dev>
2022-06-08 12:30:40 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -50,16 +50,17 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  //navigationStyle: 'default'
-   navigationStyle: 'custom',
-   navigationLinks: [
-    {
-       title: 'About',
-       pageId: '9c11186453734777b807cce609a79f45'
-     },
-     {
-       title: 'Get in touch',
-  ]     pageId: '8d4b1368360a41908918802966c097a9'
-     }
-   ]
+  navigationStyle: 'default'
+  // navigationStyle: 'custom',
+  // navigationLinks: [
+  //   {
+  //     title: 'About',
+  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
+  //   },
+  //   {
+  //     title: 'Contact',
+  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
+  //   }
+  // ]
+})
 })
