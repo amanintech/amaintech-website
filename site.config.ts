fcdaf699e5dab7c68ca5f9fc465936671f6f4c3f
Aman Sharma <amanrazdan@amp.dev>
2022-06-08 12:16:52 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,30 +2,33 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '43d0f1b7e9734ba599478789ade378cf',
+  rootNotionPageId: '2c5ddf687f8547e3af8955120604e3e3',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Researh3 | DAO ecosystem for Research',
-  domain: 'research3.xyz',
-  author: 'Research3 DAO',
+  name: 'Aman Sharma',
+  domain: process.env.NEXT_PUBLIC_DOMAIN || 'amanin.tech',
+  author: 'Aman Sharma',
+
 
   // open graph metadata (optional)
-  description: 'Decentralized research ecosystem',
+  description: 'Tech X ( Entreprenuer + Stack Advisor ) + Mentor / Speaker  | CoFounder & CTO twimbit  ',
+  socialImageTitle: 'amanin.tech',
+  socialImageSubtitle: 'Hey from Aman! 👋',
 
   // social usernames (optional)
-  twitter: '',
-  github: '',
-  linkedin: '',
+  twitter: 'amanintech',
+  github: 'amanintech',
+  linkedin: 'amanintech',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
-  defaultPageIcon: 'https://research3.xyz/_next/image?url=https%3A%2F%2Fwww.notion.so%2Fimage%2Fhttps%253A%252F%252Fs3-us-west-2.amazonaws.com%252Fsecure.notion-static.com%252F6b1c0ea3-76d0-4916-97a5-fae8efad254a%252FkwYlpxVYdbIDthEiw1mnOwV0muADvLWwSERfLvwAo5rRvbHZE5oZJdsya1FYaLgYw9rau7UldTxg6X_9um20hToSHHtbgO4VAiVIJXtLewQtjBL02Jfx83FQLQfMN7e8kwO1gYF7IHnKV2Q9YalQIEB7znfwTCYQve42A3IqXdYljWFQovH3dhFRzwUGtp14nxBLJSmju5gy8rdlYt2fGimBkPCTZMG5xespPyxD_8.jpeg%3Ftable%3Dblock%26id%3D43d0f1b7-e973-4ba5-9947-8789ade378cf%26cache%3Dv2&w=828&q=75',
+  defaultPageIcon: 'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F50b08723-06fe-4b85-b629-dcd79ad2f003%2F1638644413393.jpg?table=block&id=919c6b7b-02bf-45bb-80e4-714e27143848&cache=v2',
   defaultPageCover: null,
   defaultPageCoverPosition: 0.5,
 
