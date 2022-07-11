94ded5f614a1747483b392a1f6a088a318ca2d4e
Aman Sharma <amanrazdan@amp.dev>
2022-07-11 22:54:32 +0530

    Update contact.tsx

--- components/contact.tsx
+++ components/contact.tsx
@@ -10,8 +10,11 @@ export const contact: React.FC<types.PageProps> = ({ site, pageId, error }) => {
   return (
     <>
       <PageHead site={site} title={title} />
-
+<div className={styles.container}>
+        <main>
       <iframe width='100%' height='750px' src='https://call.amanin.tech/portal-embed#/customer/call' frameborder='0' allowfullscreen='' > </iframe>
+  </main>
+      </div>
     </>
   )
 }
